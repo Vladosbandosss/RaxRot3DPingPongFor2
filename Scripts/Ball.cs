bf014e4436d68b3816babf8f721d399bb8aecee0
Vladosbandosss <dasistperfektos@gmail.com>
2021-11-16 16:18:03 +0300

    RP
    RP

---
+++ Scripts/Ball.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private Vector3 InitialImpulse;
+
+    private Vector3 initialPosition;
+
+    [SerializeField]  private float impulseForce = 20f;
+
+    public Text Player1Scoretext, Player2Scoretext;
+    private int p1Score, p2Score;
+    private void Awake()
+    {
+        initialPosition = transform.position;
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+
+        SetScore();
+
+        if (Random.Range(0, 2) > 0)
+        {
+            impulseForce *= -1f;
+        }
+
+        InitialImpulse = new Vector3(impulseForce, 0f, impulseForce);
+
+        StartCoroutine(nameof(BallStartMovement));
+    }
+    IEnumerator BallStartMovement()
+    {
+        yield return new WaitForSeconds(2f);
+        rb.AddForce(InitialImpulse,ForceMode.Impulse);
+        
+    }
+
+    void SetScore()
+    {
+        Player1Scoretext.text = "Player1: " + p1Score;
+        Player2Scoretext.text = "Player2:" + p2Score;
+    }
+
+    void ResetMoveMent()
+    {
+        rb.velocity = Vector3.zero;
+        transform.position = initialPosition;
+        StartCoroutine(nameof(BallStartMovement));
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player1Score")
+        {
+            p2Score++;
+            SetScore();
+            ResetMoveMent();
+        }
+        if (other.tag == "Player2Score")
+        {
+            p1Score++;
+            SetScore();
+            ResetMoveMent();
+        }
+    }
+}
---
+++ Scripts/Player2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public float speed = 20f;
+
+    public float maxX = 14.7f, minX = -14.7f;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        Movement();
+        CheckBounds();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+    }
+
+    void CheckBounds()
+    {
+        Vector3 temp = transform.position;
+        if (temp.x > maxX)
+        {
+            temp.x = maxX;
+        }
+        if (temp.x < minX)
+        {
+            temp.x = minX;
+        }
+
+        transform.position = temp;
+    }
+}
